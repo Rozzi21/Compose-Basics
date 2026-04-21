1cba5d8eec58b7bc93e35e558a4ffad06cfe7552
Rozzi21 <fakhruddinarrozi@gmail.com>
2026-04-21 14:11:00 +0700

    add new function NotOuntlinedEditTextEcample

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose_basics.component.EditTextExample
+import com.example.compose_basics.component.NotOutlinedEditTextExample
 import com.example.compose_basics.ui.theme.ComposeBasicsTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +21,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeBasicsTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    EditTextExample(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                    ) {
+
+                        EditTextExample(
+                        )
+                        NotOutlinedEditTextExample(
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/compose_basics/component/UIComponents.kt
+++ app/src/main/java/com/example/compose_basics/component/UIComponents.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_basics.R
 
 
 @Composable
@@ -29,6 +32,24 @@ fun EditTextExample( modifier: Modifier = Modifier) {
         colors = TextFieldDefaults.colors(
                 focusedIndicatorColor = Color.Blue,
                 unfocusedIndicatorColor = Color.Black
+        )
+    )
+}
+
+@Composable
+fun NotOutlinedEditTextExample( modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("")}
+
+    TextField(
+        value = text,
+        onValueChange = {text = it},
+        label = {Text(stringResource(id = R.string.sample))},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, bottom = 16.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Blue,
+            unfocusedIndicatorColor = Color.Black
         )
     )
 }
