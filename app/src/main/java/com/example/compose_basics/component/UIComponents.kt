163c5ceaa7ef9de7d70234629e15fa746cccf2bb
Rozzi21 <fakhruddinarrozi@gmail.com>
2026-04-21 13:48:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/compose_basics/component/UIComponents.kt
@@ -0,0 +1,58 @@
+package com.example.compose_basics.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun EditTextExample( modifier: Modifier = Modifier) {
+
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange={ text = it},
+        label = { Text("Ini adalah teks Outline")},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Blue,
+                unfocusedIndicatorColor = Color.Black
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UIElementsPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .padding(start = 12.dp, end = 12.dp)
+    ) {
+        EditTextExample()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
